38ec74185874bae8264ece10dbc4fd4d7375f1d7
Ole Kristian Settem <ole@zmmaskin.no>
2022-12-21 21:46:54 +0100

    Hello swa cli

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,21 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = React.useState('');
+  const [message, setMessage] = React.useState('');
+
+  const getDataFromApi = async(e: any)=>{
+    e.preventDefault();
+    const data = await fetch(`/api/hello?name=${name}`);
+    const json = await data.json();
+
+    if (json.message){
+      setMessage(json.message);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Static Web App: React App with Azure Function API
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form id="form1" className="App-form" onSubmit={e => getDataFromApi(e)}>
+          <div>
+            <input 
+              type="text" 
+              id="name" 
+              className="App-input" 
+              placeholder="Name" 
+              value={name} 
+              onChange={e=>setName(e.target.value)} />
+            <button type="submit" className="App-button">Submit</button>
+          </div>
+        </form>
+        <div><h5>Message: {message} </h5></div>
       </header>
     </div>
   );
