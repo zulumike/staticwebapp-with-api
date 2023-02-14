05dafe19873f116e798ada893cecdf82d4095995
Ole Kristian Settem <ole@zmmaskin.no>
2023-02-14 19:09:23 +0100

    Added authentication demo(without password)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,45 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+
+// Custom components
+import NavBar from './components/NavBar';
+import PublicHome from './components/PublicHome';
+import PrivateHome from './components/PrivateHome';
 
 function App() {
 
-  const [name, setName] = React.useState('');
-  const [message, setMessage] = React.useState('');
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
 
-  const getDataFromApi = async(e: any)=>{
-    e.preventDefault();
-    const data = await fetch(`/api/hello?name=${name}`);
-    const json = await data.json();
+  useEffect(() => {
+    getUserInfo();
+  }, []);
 
-    if (json.message){
-      setMessage(json.message);
+  async function getUserInfo() {
+    try {
+
+        const response = await fetch('/.auth/me');
+        const payload = await response.json();
+        const { clientPrincipal } = payload;
+        
+        if(clientPrincipal){
+          setUser(clientPrincipal);
+          userHasAuthenticated(true);
+          console.log(`clientPrincipal = ${JSON.stringify(clientPrincipal)}`);
+        } 
+        
+    } catch (error:any) {
+        console.error('No profile could be found ' + error?.message?.toString());
     }
-  };
+};  
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Static Web App: React App with Azure Function API
-        </p>
-        <form id="form1" className="App-form" onSubmit={e => getDataFromApi(e)}>
-          <div>
-            <input 
-              type="text" 
-              id="name" 
-              className="App-input" 
-              placeholder="Name" 
-              value={name} 
-              onChange={e=>setName(e.target.value)} />
-            <button type="submit" className="App-button">Submit</button>
-          </div>
-        </form>
-        <div><h5>Message: {message} </h5></div>
-      </header>
+      <NavBar user={user}/>
+      <main className="column">
+        { isAuthenticated ? <PrivateHome user={user}/> : <PublicHome /> }
+      </main>
     </div>
-  );
+  )
 }
 
 export default App;
